59ff6f6ae798639d2f4fc0b3da52b10243d490bf
Sandro Gallina <sandro_gallina@hotmail.com>
2018-02-11 19:13:28 -0200

    Atualizacao:utilizando somente JsonIgnore

--- src/main/java/com/sandrogallina/cursomc/domain/Endereco.java
+++ src/main/java/com/sandrogallina/cursomc/domain/Endereco.java
@@ -9,8 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
+ 
 @Entity
 public class Endereco implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -23,8 +23,8 @@ public class Endereco implements Serializable{
 	private String complemento;
 	private String bairro;
 	private String cep; 
-	
-	@JsonBackReference
+
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
