4f369fe937af8329f13e027f2cbf27557472a58d
Sandro Gallina <sandro_gallina@hotmail.com>
2018-02-11 15:52:29 -0200

    Crianco operacao de instanciacao

--- src/main/java/com/sandrogallina/cursomc/CursomcApplication.java
+++ src/main/java/com/sandrogallina/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.sandrogallina.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.sandrogallina.cursomc.domain.Categoria;
+import com.sandrogallina.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informaática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 }
